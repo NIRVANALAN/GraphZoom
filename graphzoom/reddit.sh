84b769979d44d0fd801a00f67a71c45585d1a93b
NIRVANALAN <maverickers@outlook.com>
2020-03-10 11:52:30 +0000

    update r

--- graphzoom/reddit.sh
+++ graphzoom/reddit.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-for i in `seq 1 4`    ## coarsening level
+for i in `seq 1 5`    ## coarsening level
     do
         ratio=$(case "$i" in
-            (1)  echo 2;;
-            (2)  echo 5;;
-            (3)  echo 9;;
-            (4)  echo 19;;
+            (1)  echo 60;;
+            (2)  echo 27;;
+            (3)  echo 19;;
+            (4)  echo 9;;
+            (5)  echo 5;;
         esac)
         # python graphzoom.py -r ${ratio} -m deepwalk
-        # python graphzoom.py -r ${ratio} -m deepwalk -pre /yushi/ -d reddit
-        python graphzoom.py -r ${ratio} -m deepwalk -pre /mnt/yushi/ -d reddit
+        python graphzoom.py -r ${ratio} -m deepwalk -pre /yushi/ -d reddit
+        # python graphzoom.py -r ${ratio} -m deepwalk -pre /mnt/yushi/ -d reddit
 done
