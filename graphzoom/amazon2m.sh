3b4c4c297d84a98f377ca7a4f120dc18cf568141
NIRVANALAN <maverickers@outlook.com>
2020-03-10 17:16:12 +0000

    update 2m

--- graphzoom/amazon2m.sh
+++ graphzoom/amazon2m.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-for i in `seq 4 6`    ## coarsening level
+for i in `seq 4 8`    ## coarsening level
     do
         ratio=$(case "$i" in
-            (1)  echo 2;;
+            (1)  echo 12;; #4
             (2)  echo 5;;
             (3)  echo 9;;
             (4)  echo 90;;
             (5)  echo 60;;
             (6)  echo 27;;
-            (6)  echo 9;;
+            (7)  echo 19;;
+            (8)  echo 9;;
         esac)
         python graphzoom.py -r ${ratio} -m deepwalk -d Amazon2M -pre  /yushi
 done
